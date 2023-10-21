227ebf8ca46fb1e4efeadf56c74ef176f5032bdb
CaioSouzaAds <caio.cesarads@gmail.com>
2023-10-21 17:01:30 -0300

    login page created

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,98 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+
+  function handleLogin() {
+    if (email === "" || password === "") {
+      return;
+    }
+  }
+
   return (
-    <View>
-      <Text>Tela de login</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../../assets/newlogo.png")}
+        resizeMode="contain"
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu email"
+          style={styles.input}
+          placeholderTextColor="#F0F0F0"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          placeholder="Sua password"
+          style={styles.input}
+          placeholderTextColor="#F0F0F0"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setpassword(text)}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d1d2e",
+  },
+
+  logo: {
+    width: "70%",
+    height: 200,
+    marginBottom: 0,
+    marginTop: -200,
+  },
+  inputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  input: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#101026",
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: "#FFF",
+  },
+  button: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#101026",
+  },
+});
