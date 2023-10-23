12f1243c0191e429cb426aa894162f21d5535bb7
CaioSouzaAds <caio.cesarads@gmail.com>
2023-10-23 14:57:27 -0300

    created new order screen

--- mobile/src/pages/DashBoard/index.tsx
+++ mobile/src/pages/DashBoard/index.tsx
@@ -1,5 +1,12 @@
 import React, { useContext } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 import { AuthContext } from "../../contexts/AuthContex";
 
@@ -7,9 +14,60 @@ export default function DashBoard() {
   const { signOut } = useContext(AuthContext);
 
   return (
-    <View>
-      <Text>Screm DashBoard</Text>
-      <Button title="Sair do app" onPress={signOut} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Novo pedido</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Número da mesa"
+        placeholderTextColor="#F0F0F0"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Abrir mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 5,
+    backgroundColor: "#1d1d2e",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#F0F0F0",
+    marginBottom: 24,
+    marginTop: -100,
+  },
+  input: {
+    width: "90%",
+    height: 60,
+    backgroundColor: "#101026",
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    fontSize: 22,
+    color: "#F0F0F0",
+  },
+  button: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#3FFFA3",
+    borderRadius: 4,
+    marginVertical: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#101026",
+    fontWeight: "bold",
+  },
+});
